4e0588c9b22d33450750103cea1b2dac28982618
Temp-37538 <193842841+Temp-37538@users.noreply.github.com>
2025-11-07 00:58:20 +0100

    feat: refactor FullPageView pour améliorer la mise en page et l'affichage de l'image

--- apps/web/src/components/full-image-page.tsx
+++ apps/web/src/components/full-image-page.tsx
@@ -2,5 +2,18 @@ import { getImage } from "../../server/queries";
 
 export default async function FullPageView(props: { id: number }) {
   const image = await getImage(props.id);
-  return <img className="w-96" src={image?.url} alt={image?.name} />;
+  return (
+    <div className="flex h-full w-full min-w-0 ">
+      <div className="shrink flex justify-center items-center p-4">
+        <img
+          className="w-96 object-contain"
+          src={image?.url}
+          alt={image?.name}
+        />
+      </div>
+      <div className="w-50 flex h-fit flex-col border-l">
+        <h1 className="text-2xl font-bold mb-4">{image?.name}</h1>
+      </div>
+    </div>
+  );
 }
