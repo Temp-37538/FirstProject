a5b043e4bd1e01348191e654291a02cca9144d03
Temp-37538 <193842841+Temp-37538@users.noreply.github.com>
2025-11-06 03:10:13 +0100

    Refactor : suppression du bouton de téléchargement dans l'en-tête et intégration dans le menu utilisateur server-side

--- apps/web/src/components/header.tsx
+++ apps/web/src/components/header.tsx
@@ -1,18 +1,12 @@
 "use client";
-import { authClient } from "@/lib/auth-client";
-import { UploadButton } from "@/utils/uploadthing";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
 import { ModeToggle } from "./mode-toggle";
 import UserMenu from "./user-menu";
 
 export default function Header() {
   const links = [{ to: "/", label: "Home" }] as const;
-  const router = useRouter();
 
-  const { data: session } = authClient.useSession();
-
-  return (
+	return (
     <div>
       <div className="flex flex-row items-center justify-between p-4">
         <nav className="flex gap-4 text-lg">
@@ -25,22 +19,8 @@ export default function Header() {
           })}
         </nav>
         <div className="flex items-center gap-2">
-          {session && (
-            <UploadButton
-              endpoint="imageUploader"
-              onClientUploadComplete={(res) => {
-                // Do something with the response
-                router.refresh();
-                alert("Upload Completed");
-              }}
-              onUploadError={(error: Error) => {
-                // Do something with the error.
-                alert(`ERROR! ${error.message}`);
-              }}
-            />
-          )}
-          <ModeToggle />
           <UserMenu />
+          <ModeToggle />
         </div>
       </div>
       <hr />
